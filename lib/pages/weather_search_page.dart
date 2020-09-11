f4849505ba4f2b6efcc73ab3cededd2699ba19ce
Fonkam Loïc <fonkamloic@gmail.com>
2020-09-11 05:35:17 +0100

    lib/pages/weather_search_page

---
+++ lib/pages/weather_search_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:state_rebuilder/data/model/weather.dart';
+import 'package:state_rebuilder/data/weather_repository.dart';
+import 'package:state_rebuilder/state/weather_store.dart';
+import 'package:states_rebuilder/states_rebuilder.dart';
+
+class WeatherSearchPage extends StatefulWidget {
+  @override
+  _WeatherSearchPageState createState() => _WeatherSearchPageState();
+}
+
+class _WeatherSearchPageState extends State<WeatherSearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Weather Search"),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 16),
+        alignment: Alignment.center,
+        child: StateBuilder<WeatherStore>(
+//  Injector.getAsReactive<WeatherStore>(),
+          builder: (_, reactiveModel) {
+            return reactiveModel.whenConnectionState(
+              onIdle: () => buildInitialInput(),
+              onWaiting: () => buildLoading(),
+              onData: (store) => buildColumnWithData(store.weather),
+              onError: (_) => buildInitialInput(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget buildInitialInput() {
+    return Center(
+      child: CityInputField(),
+    );
+  }
+
+  Widget buildLoading() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Column buildColumnWithData(Weather weather) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Text(
+          weather.cityName,
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        Text(
+          // Display the temperature with 1 decimal place
+          "${weather.temperatureCelsius.toStringAsFixed(1)} °C",
+          style: TextStyle(fontSize: 80),
+        ),
+        CityInputField(),
+      ],
+    );
+  }
+}
+
+class CityInputField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      child: TextField(
+        onSubmitted: (value) => submitCityName(context, value),
+        textInputAction: TextInputAction.search,
+        decoration: InputDecoration(
+          hintText: "Enter a city",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+          suffixIcon: Icon(Icons.search),
+        ),
+      ),
+    );
+  }
+
+  void submitCityName(BuildContext context, String cityName) {
+    final reactiveModel = Injector.getAsReactive<WeatherStore>();
+    reactiveModel.setState((s) => s.getWeather(cityName),
+        onError: (context, error) {
+      if (error is NetworkError) {
+        Scaffold.of(context).showSnackBar(SnackBar(
+          content: Text("Couldn't fetch weather. Is device online?"),
+        ));
+      } else {
+        throw error;
+      }
+    });
+  }
+}
